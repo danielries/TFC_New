c9c35c560de5f9e32c0f38e409c0147c2a9ed379
danielries <s214641@dtu.dk>
2023-06-14 15:50:30 +0200

    restart

--- hpc/run-scripts/Run.sh
+++ hpc/run-scripts/Run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #BSUB -J Sl_Ep_p
 ## ps (pretrain-subset), f (tinetune-nosubset)
-#BSUB -o hpc/runs/Run_%J.out.txt
-#BSUB -e hpc/runs/Run_%J.err.txt
+#BSUB -o ../../hpc/runs/Run_%J.out.txt
+#BSUB -e ../../hpc/runs/Run_%J.err.txt
 
 ## GPU
 #BSUB -q gpuv100
@@ -13,7 +13,7 @@
 
 ## specs
 #BSUB -R "rusage[mem=12GB] span[hosts=1]"
-#BSUB -n 6
+#BSUB -n 4
 
 ## mail when done
 #BSUB -N
@@ -23,13 +23,13 @@
 
 module load python3/3.9.11
 
-source ../HPC_env/bin/activate
+source ../../../PWenv/bin/activate
 
 ## SleepEEG -> Epilepsy (n2: 2hr)
-python code/main.py --training_mode pre_train --run_description Run1 --pretrain_dataset SleepEEG --target_dataset Epilepsy --subset False --device cuda
+python main.py --training_mode pre_train --run_description Run1 --pretrain_dataset SleepEEG --target_dataset Epilepsy --subset False --device cuda
 ## FD_A -> FD_B
-# python code/main.py --training_mode fine_tune_test --pretrain_dataset FD_A --target_dataset FD_B --subset False --device cuda
+# python main.py --training_mode pre_train --run_description Run1 --pretrain_dataset FD_A --target_dataset FD_B --subset False --device cuda
 ## ECG -> EMG (>8GB pre)
-# python code/main.py --training_mode pre_train --pretrain_dataset ECG --target_dataset EMG --subset False --device cuda
+# python main.py --training_mode pre_train --run_description Run1 --pretrain_dataset ECG --target_dataset EMG --subset False --device cuda
 ## HAR -> Gesture (pre_n2: 5hr)
-#python code/main.py --training_mode pre_train --pretrain_dataset HAR --target_dataset Gesture --subset False --device cuda
+#python main.py --training_mode pre_train --run_description Run1 --pretrain_dataset HAR --target_dataset Gesture --subset False --device cuda
